38c5c25c5877c3e96aacda9f533dff345d9efbcf
danieltanyi <tanyi_daniel@yahoo.com>
2023-11-30 05:37:49 +0100

    productController test class added

---
+++ Test/com/example/produktapi/controller/ProductControllerTest.java
@@ -0,0 +1,38 @@
+package com.example.produktapi.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void hello() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getAllCategories() {
+    }
+
+    @Test
+    void getProductsByCategory() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+}
